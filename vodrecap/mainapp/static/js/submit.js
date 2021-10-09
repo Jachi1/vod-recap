b1096c2e2e4672459048f34736e1678e9a5a8be5
Jace <jach1@umbc.edu>
2021-10-08 23:22:20 -0400

    Added js scripts for extracting values from DOM

---
+++ vodrecap/mainapp/static/js/submit.js
@@ -0,0 +1,20 @@
+// TODO:
+// 1. Sanitize input
+// 2. Send input to API
+// 3. Store input from API as JSON
+// 4. Create Visualizations
+
+
+function submit_url() {
+    // Fetch input in #search-box DOM element
+    url= $("#search-box").val();
+    console.log(url);
+}
+
+function sanitize_url(url) {
+    return
+}
+
+function submit_url_to_api(url) {
+    return
+}
