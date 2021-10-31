37fe9b3c02205075100cdf6c25a3d0034be3724d
Jace <jach1@umbc.edu>
2021-10-31 03:33:59 -0400

    Added env variable for changing debug mode

--- vodrecap/settings.py
+++ vodrecap/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if str(os.getenv('DEBUG'))=="T" else False
 
 ALLOWED_HOSTS = []
 
